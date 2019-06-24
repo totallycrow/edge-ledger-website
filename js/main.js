cac51a6de7b02c33403554a15778048e62c05cfe
Adrian Gajda <adrian.gajda@hotmail.com>
2019-06-24 21:48:47 +0100

    Added Location Map - google maps api

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function initMap() {
+    // Your Location
+    const loc = { lat: 42.361145, lng: -71.057083};
+    // Centered map on location
+    const map = new google.maps.Map(document.querySelector('.map')
+    , {
+        zoom: 14,
+        center: loc
+    });
+
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({position: loc, map: map}); 
+
+
+
+
+
+
+}
